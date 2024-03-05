621a94829ca8d1a4b6db377d1403fc26cde905e2
Chakravarthi <vkchakravarthi15@gmail.com>
2024-03-05 12:34:19 +0530

    Add project files.

---
+++ Data/RegistrationFormContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using RegistrationForm.Models;
+
+namespace RegistrationForm.Data
+{
+    public class RegistrationFormContext : DbContext
+    {
+        public RegistrationFormContext (DbContextOptions<RegistrationFormContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<RegistrationForm.Models.UserModel> UserModel { get; set; } = default!;
+    }
+}
